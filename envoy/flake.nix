e547e717a3a2b148ca17601d20c3e647b53cf904
Colton J. McCurdy <mccurdyc22@gmail.com>
2025-07-02 08:15:10 -0400

    adds com_github_wasmtime patch
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- envoy/flake.nix
+++ envoy/flake.nix
@@ -17,12 +17,22 @@
 
       perSystem = { system, ... }:
         let
+          # https://github.com/envoyproxy/envoy/blob/e439c73e32dfefff0baa4adedfb268c8742a7617/bazel/repository_locations.bzl#L1161-L1175
+          # https://github.com/hsjobeki/nixpkgs/blob/43bceee4fd57058437d9ec90eae7c1b280509653/pkgs/build-support/fetchgithub/default.nix#L4
+          com_github_wasmtime = pkgs.fetchFromGitHub {
+            owner = "bytecodealliance";
+            repo = "wasmtime";
+            # 24.0.2 - https://github.com/bytecodealliance/wasmtime/releases/tag/v24.0.2
+            rev = "c29a9bb9e23b48a95b0a03f3b90f885ab1252a93";
+            sha256 = "sha256-76a5eedf3d57de8a97492006cfa9c2c5eedf81ad82ba173f0615e85695cecdf7";
+          };
+
           pkgs = import inputs.nixpkgs {
             inherit system;
             config.allowUnfree = true;
 
             overlays = [
-              (self: super: {
+              (_: super: {
 
                 # We use overrideDerivation even though it's strongly advised against in the nixpkgs
                 # manual because we need to override nativeBuildInputs with dependencies that are
@@ -32,19 +42,24 @@
                 # Evaluates a derivation before modifying it
                 #
                 # The function overrideDerivation creates a new derivation based on an existing one by overriding the original’s attributes with the attribute set produced by the specified function. This function is available on all derivations defined using the makeOverridable function. Most standard derivation-producing functions, such as stdenv.mkDerivation, are defined using this function, which means most packages in the nixpkgs expression, pkgs, have this function.
-                envoy = super.envoy.overrideDerivation (oldAttrs: {
-                  stdenv = super.stdenv;
+                envoy = super.envoy.overrideDerivation (_: {
+                  inherit (super) stdenv;
 
-                  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-                    pkgs.wasmtime
-                  ];
+                  # https://github.com/mccurdyc/envoy/blob/6371b185dee99cd267e61ada6191e97f2406e334/api/bazel/envoy_http_archive.bzl#L4-L9 
+                  # Envoy's Bazel WONT fetch repos that are listed in the existing_rules list
 
-                  #patches = [
                   # Something that replaces Bazel's com_github_wasmtime so that bazel doesnt try to install inside
                   # the build vm and instead uses either pkg.wasmtime if it
                   # just needs a binary OR we'll have to use fetchFromGitHub and
                   # probably put in the bazel build dir.
-                  #];
+                  patches = [
+                    ./patches/0001-com_github_wasmtime_from_nix.patch
+                  ];
+
+                  postPatch = ''
+                    # https://nixos.org/manual/nixpkgs/unstable/#fun-substitute
+                    substituteInPlace WORKSPACE --subst-var-by com_github_wasmtime_from_nix ${com_github_wasmtime}
+                  '';
 
                   wasmRuntime = "wasmtime";
                 });
