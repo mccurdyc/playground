664467be5021c58b978b639adc7cbd0eec951a20
Colton J. McCurdy <mccurdyc22@gmail.com>
2025-08-22 07:59:54 -0400

    [wasm] minimal envoy response headers
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -22,18 +22,10 @@ struct HelloWorld {
 impl wasm::traits::Context for HelloWorld {}
 
 impl wasm::traits::HttpContext for HelloWorld {
-    fn on_http_request_headers(&mut self, num_headers: usize, _: bool) -> wasm::types::Action {
+    fn on_http_response_headers(&mut self, num_headers: usize, _: bool) -> wasm::types::Action {
         info!("Got {} HTTP headers in #{}.", num_headers, self.context_id);
-        let headers = self.get_http_request_headers();
-        let mut authority = "";
 
-        for (name, value) in &headers {
-            if name == ":authority" {
-                authority = value;
-            }
-        }
-
-        self.set_http_request_header("x-hello", Some(&format!("Hello world from {}", authority)));
+        self.set_http_response_header("x-hello", Some("Hello from WASM!"));
 
         wasm::types::Action::Continue
     }
