45ba051272d1e6944b4f64147274a967a4f07fe0
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-01-25 11:56:14 -0500

    nix template
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

---
+++ asm/programming-from-the-ground-up/5_params/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          nasm
+          unixtools.xxd
+
+          shfmt
+
+          # nix
+          deadnix
+          statix
+          nil
+          nixpkgs-fmt
+        ];
+        # libraries
+        buildInputs = [ ];
+      };
+    };
+}
--- asm/programming-from-the-ground-up/5_params/params.s
+++
@@ -1,9 +0,0 @@
-; USAGE: params 1 2
-
-; need a buffer for two unsigned int8
-section .bss
-
-section .text
-
-global _start
-_start:
