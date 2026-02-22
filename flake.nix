41c42eb2466f9e1b8b69e82fb97ba4bbf967a191
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-02-22 15:19:16 -0500

    working submodule flakes
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,11 +4,10 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # https://lazamar.co.uk/nix-versions/?package=yarn&version=1.22.19&fullName=yarn-1.22.19&keyName=yarn&revision=336eda0d07dc5e2be1f923990ad9fdb6bc8e28e3&channel=nixpkgs-unstable#instructions
     # nixpkgs-foo.url = "https://github.com/NixOS/nixpkgs/archive/336eda0d07dc5e2be1f923990ad9fdb6bc8e28e3.tar.gz";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-parts, pre-commit-hooks, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake = { };
 
@@ -58,38 +57,9 @@
 
           formatter = pkgs.nixpkgs-fmt;
 
-          # https://github.com/cachix/git-hooks.nix
-          # 'nix flake check'
-          checks = {
-            pre-commit-check = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                # Nix
-                deadnix.enable = true;
-                nixpkgs-fmt.enable = true;
-                statix.enable = true;
-
-                # Shell
-                shellcheck = {
-                  enable = true;
-                  # exclude exactly .envrc anywhere
-                  excludes = [ "\\.envrc$" ];
-                  # or only check *.sh files
-                  # files = "\\.sh$";
-                };
-                shfmt = {
-                  enable = true;
-                  entry = "shfmt --simplify --indent 2";
-                };
-              };
-            };
-          };
-
           packages = ci_packages;
 
           devShells.default = pkgs.mkShell {
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
-            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
             inherit packages;
           };
         };
--- rust/machine-code/flake.nix
+++ rust/machine-code/flake.nix
@@ -11,7 +11,7 @@
     rust-flake.url = "github:juspay/rust-flake";
 
     # personal preferences
-    # mccurdyc-preferences.url = "path:../modules";
+    # mccurdyc-preferences.url = "path:../../nix-templates/modules";
     mccurdyc-preferences.url = "github:mccurdyc/nix-templates?dir=modules";
   };
 
@@ -23,14 +23,69 @@
       imports = [
         inputs.git-hooks.flakeModule
         inputs.mccurdyc-preferences.flakeModules.default
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
       ];
 
-      perSystem = _: {
+      perSystem = { pkgs, ... }: {
+        rust-project.crates."app" = {
+          crane = {
+            args = {
+              nativeBuildInputs = [ pkgs.just ];
+            };
+            extraBuildArgs = {
+              buildPhaseCargoCommand = "just build";
+              checkPhaseCargoCommand = "just test";
+            };
+          };
+        };
+
         mccurdyc = {
           pre-commit = {
             enable = true;
+            rootDir = "rust/machine-code";
             rust.enable = true;
             just.enable = true;
+          };
+
+          devshell = {
+            enable = true;
+            extraPackages = [ ];
+          };
+
+          dockerfile = {
+            enable = true;
+            extraIgnore = ''
+              target/
+            '';
+            content = ''
+              # syntax=docker/dockerfile:1
+              FROM lukemathwalker/cargo-chef:latest-rust-1.87-alpine AS chef
+              WORKDIR /app
+
+              FROM chef AS planner
+              COPY . .
+              RUN cargo chef prepare --recipe-path recipe.json
+
+              FROM chef AS builder
+              COPY --from=planner /app/recipe.json recipe.json
+              # Build dependencies - this is the caching Docker layer!
+              RUN cargo chef cook --release --recipe-path recipe.json
+              # Build application
+              COPY . .
+              RUN apk add --no-cache just
+              RUN just build
+
+              FROM alpine:3.20 AS runtime
+              RUN apk add --no-cache ca-certificates \
+                  && addgroup -g 1000 app \
+                  && adduser -D -s /bin/sh -u 1000 -G app app
+              WORKDIR /app
+              COPY --from=builder /app/target/release/app /app/app
+              USER app
+              ENTRYPOINT ["/app/app"]
+              CMD ["greet"]
+            '';
           };
         };
       };
